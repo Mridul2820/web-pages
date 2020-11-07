8e1de192a3d1ffff6ad639faaff9fcc695b66058
Mridul2820 <immridulpanda2820@gmail.com>
2020-11-07 20:07:29 +0530

    Starbucks Landing Page

---
+++ the-pages/starbucks-landing-page/script.js
@@ -0,0 +1,8 @@
+function imgSlider(img) {
+    document.querySelector('.starbucks').src = img;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
